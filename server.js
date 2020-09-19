f2b53b5fa4346d3e0250f682eae2d9f4589d2077
Brianna LaCasse <briannalacasse@Briannas-MacBook-Pro.local>
2020-09-18 20:06:17 -0400

    got data in mongo

--- server.js
+++ server.js
@@ -18,22 +18,22 @@ app.get('/', (req, res) => res.send("This is root!"))
 
 app.get('/posts', async (req, res) => {
     try {
-        const posts = await Post.find() // this is a mongoose thing
-        res.json(posts)
+      const posts = await Post.find() // this is a mongoose thing
+      res.json(posts)
     } catch (error) {
-        res.status(500).json({ error: error.message })
+      res.status(500).json({ error: error.message })
     }
 })
 
 app.get('/posts/:id', async (req, res) => {
     try {
-        const { id } = req.params
-        const post = await Post.findById(id)
-        if (!post) throw Error('Post not found')
-        res.json(post)
-    } catch (e) {
-        console.log(e)
-        res.send('Post not found!')
+      const { id } = req.params
+      const post = await Post.findById(id)
+      if (!post) throw Error('Post not found')
+      res.json(post)
+  } catch (e) {
+      console.log(e)
+      res.send('Post not found!')
     }
 })
 
